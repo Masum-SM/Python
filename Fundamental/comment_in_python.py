0a21d6a15399cf25e2f9e46edcd1d2291fdc4508
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2022-09-30 10:41:55 +0600

    Add files via upload

---
+++ Fundamental/comment_in_python.py
@@ -0,0 +1,13 @@
+
+""" 
+Single line : control + /
+Multi-line : alt + shift + a
+"""
+print("single Line Comment")
+# to comment any single line in python, the '#' symbol is used before line. (control + / )is used to as shortcut key for making single line comment .
+
+print("Multi Line Comment");
+"""
+For making multiline comment in python, here the (""" """) tripple double cottation is used.
+(alt + shift + a) is used as shortcut key for creating multiline comment. 
+"""
