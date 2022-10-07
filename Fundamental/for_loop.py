00bc6b18d29b66b4fc6d143ad1e516bb951bed01
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2022-10-07 19:09:32 +0600

    Add files via upload

---
+++ Fundamental/for_loop.py
@@ -0,0 +1,21 @@
+My_name = "Masum"
+for char in My_name:
+    print(char)
+
+print("\n")
+
+
+for indx in range(11):
+    print(indx)
+
+print("\n")
+
+for n in range(5,15):
+    print(n)
+
+
+print("\n")
+
+for n in range(5,15,2):
+    print(n)
+
