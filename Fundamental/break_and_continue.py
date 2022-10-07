36e5b56952588dba677346152676290be0464c2e
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2022-10-07 19:09:14 +0600

    Add files via upload

---
+++ Fundamental/break_and_continue.py
@@ -0,0 +1,14 @@
+num = 10
+while num < 100:
+    print (num)
+    if(num == 70):
+        break;
+    num += 10
+print("\n\n")
+n = 0
+while n <=10:
+    n +=1
+    if n % 2 == 1:
+        continue
+    print(n)
+    
