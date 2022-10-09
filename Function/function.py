b0e9ca5165d8ba120f6d70f08ee5dd3ed53e8de4
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2022-10-09 11:18:42 +0600

    Add files via upload

---
+++ Function/function.py
@@ -0,0 +1,25 @@
+
+def add(num1, num2):
+    result = num1 + num2
+    print(result)
+
+add(4,7)
+
+def substruction(x , y):
+    sub = x - y
+    print(sub)
+substruction(y= 3, x = 7)
+
+
+def multiplication(n , m):
+    mul = n * m
+    return mul
+
+result = multiplication(3 , 4)
+print(result)
+
+def division(num1 , num2 = 2):
+    div = num1 / num2
+    return div
+
+print(division(10))
