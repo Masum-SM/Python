c6c51f69a4e893502e21dffa455a6eca6f1cb7b8
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2022-10-10 23:19:27 +0600

    Add files via upload

---
+++ Function/autogui_package.py
@@ -0,0 +1,10 @@
+import pyautogui
+# pyautogui.alert('unus masum')
+
+import time
+for i in range(1,20):
+  time.sleep(1)
+  pyautogui.write('Tomra ki Jolshiri jaba na?', interval=0.25)  
+  pyautogui.press('enter') 
+
+
