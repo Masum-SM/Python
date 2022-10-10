3c532761b480c867d0423722293e0bb9b6360467
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2022-10-10 23:19:58 +0600

    Add files via upload

---
+++ Function/build_in_modules.py
@@ -0,0 +1,20 @@
+from math import ceil,floor
+c_result = ceil(4.5)
+print(c_result)
+f_result = floor(4.5)
+print(f_result)
+
+import random
+num = random.random()
+# It will give a random number between 0 to 1.
+print(num)
+
+int_num = random.randint(20,50)
+print(int_num)
+
+choice_num = random.choice([2,30,23,56,30,40])
+print(choice_num)
+
+import datetime
+today = datetime.date(2022,10,8)
+print(today)
