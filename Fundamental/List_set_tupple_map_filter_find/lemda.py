eaa1f8506a07d20d23c297cbb49164864849b9ff
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2022-10-11 23:12:44 +0600

    Add files via upload

---
+++ Fundamental/List_set_tupple_map_filter_find/lemda.py
@@ -0,0 +1,22 @@
+def squre(x):
+    return x*x
+result = squre(6)
+print(result)
+
+def additon(x,y):
+    return x+y
+add_result = additon(6,5)
+print(add_result)
+
+# =================== lambda function =======================
+
+
+# fun_name = lemda parameters : equation
+finding_sum = lambda x,y : x+y
+ans = finding_sum(50,2)
+print(ans)
+
+
+finding_sqr = lambda x : x*x
+sqr_ans = finding_sqr(7)
+print(sqr_ans)
