567f2285947f19f55ceb34a3aadd42896285efc2
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2022-10-12 22:41:28 +0600

    Add files via upload

---
+++ Fundamental/List_set_tupple_map_filter_find/dictonary.py
@@ -0,0 +1,10 @@
+marks = {'Math':12, 'Physics':23,'Chemistry':26,'Biology':22}
+print(marks)
+marks['ICT']  = 25
+print(marks)
+marks_keys = marks.keys()
+print(marks_keys)
+marks_value = marks.values();
+print(marks_value)
+marks_item = marks.items()
+print(marks_item)
