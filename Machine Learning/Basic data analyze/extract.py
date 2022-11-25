ac2bb1f20612c9c36d88a116e2aa9dd6b7a876bf
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2022-11-25 10:46:19 +0600

    Add files via upload

---
+++ Machine Learning/Basic data analyze/extract.py
@@ -0,0 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import csv
+
+
+url = 'https://en.wikipedia.org/wiki/IPhone'
+text = requests.get(url).text.encode('utf-8').decode('ascii','ignore')
+soup = BeautifulSoup(text,'lxml') #lxml is a parser
+table = soup.find('table',class_= 'wikitable')
+rows = table.find_all('tr')[1:]
+
+iphone_price_dict = {}
+
+for row in rows:
+    data = row.find_all(['th','td'])
+    try:
+        version_text = data[0].a.text.split(' ')[1]
+        version = re.sub(r"\D","",version_text)
+        # print(version)
+        price_text = data[-1].text.split('/')[-1]
+        price = re.sub(r"\D","",price_text)
+        price = int(price)
+        if version and price >100 : 
+            iphone_price_dict[version] = price
+
+    except:
+        pass
+print(iphone_price_dict)
+csv_fields = ['version','price']
+
+with open('iphone_price.csv','w',newline='') as csvFile:
+    csvwriter = csv.writer(csvFile)
+    csvwriter.writerow(csv_fields)
+    for key,value in iphone_price_dict.items():
+        csvwriter.writerow([key,value])
+    csvFile.close()
