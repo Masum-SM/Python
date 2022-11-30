26b462ce46f27d7d93fddd877c5d2e209dcef9f1
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2022-11-30 22:35:27 +0600

    Update Lamda.py

--- Fundamental/Recap/Lamda.py
+++ Fundamental/Recap/Lamda.py
@@ -7,3 +7,17 @@ greed()
 # using lambda funcion
 greed_ = lambda : print("hi every one")
 greed_()
+
+
+
+# ---------------> EXAMPLE 02<-------------
+def max_(a,b):
+    if a>b:
+        return a
+    else:
+        return b
+print(max_(6,8))
+
+# using lambda funcion
+_max = lambda a,b: a if a>b else b
+print(_max(3,5))
