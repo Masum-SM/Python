68d3a222fc786b8a219fa901c11b344a9a9b03fd
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2022-12-01 12:39:41 +0600

    Update List comprehension.py

--- Fundamental/Recap/List comprehension.py
+++ Fundamental/Recap/List comprehension.py
@@ -1,1 +1,8 @@
 
+# ------------------------> Example 01 : upper case <------------------------
+
+str_list = ['masum','nawrin','sultana','unus']
+new_lst = [x.upper() for x in str_list ]
+print(new_lst)
+
+# output : ['MASUM', 'NAWRIN', 'SULTANA', 'UNUS']
