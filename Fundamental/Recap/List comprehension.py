fd495cc1f703732f975681854fc9d309ad52dc45
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2022-12-01 12:40:20 +0600

    Update List comprehension.py

--- Fundamental/Recap/List comprehension.py
+++ Fundamental/Recap/List comprehension.py
@@ -6,3 +6,17 @@ new_lst = [x.upper() for x in str_list ]
 print(new_lst)
 
 # output : ['MASUM', 'NAWRIN', 'SULTANA', 'UNUS']
+
+
+# ------------------------> Example 02 : sequence of number <------------------------
+
+x = [i for i in range(1,10)]
+print(x)
+y = list(range(1,10))
+print(y)
+
+""" 
+output : 
+[1, 2, 3, 4, 5, 6, 7, 8, 9]
+[1, 2, 3, 4, 5, 6, 7, 8, 9] 
+"""
