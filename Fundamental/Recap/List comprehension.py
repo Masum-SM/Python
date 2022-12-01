5eabb24551e7161e28c8ae8e28b0fc4797c77da1
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2022-12-01 12:42:25 +0600

    Update List comprehension.py

--- Fundamental/Recap/List comprehension.py
+++ Fundamental/Recap/List comprehension.py
@@ -34,3 +34,21 @@ print(list(s))
 ['N', 'a', 'w', 'r', 'i', 'n']
 ['N', 'a', 'w', 'r', 'i', 'n']
 """
+
+
+# ------------------------> Example 04 : sequence of even number <------------------------
+
+num_lst = [1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15]
+
+even = [n for n in num_lst if n%2 == 0]
+print(even)
+
+even_range = [n for n in range(1,16) if n%2 == 0]
+print(even_range)
+
+"""  
+output : 
+[2, 4, 6, 8, 10, 12, 14]
+[2, 4, 6, 8, 10, 12, 14]
+"""
+
