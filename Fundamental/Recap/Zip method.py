62b2bc9e883f8b5453f716b543b62911efa48216
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2022-12-09 11:50:10 +0600

    Update Zip method.py

--- Fundamental/Recap/Zip method.py
+++ Fundamental/Recap/Zip method.py
@@ -10,3 +10,10 @@ print(list(new_obj))
 # output : [(1, 'one', 1.1), (2, 'two', 2.2), (3, 'three', 3.3)]
 
 
+
+# ---------------------------------- Example 02 -------------------------
+
+names = ['masum','karim','halim']
+salaries = [10000,20000,30000]
+print(list(zip(names,salaries)))
+# output : [('masum', 10000), ('karim', 20000), ('halim', 30000)]
