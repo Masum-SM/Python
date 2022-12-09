a40cb9a20f9b1b2c00e06101fcf7910ecee70439
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2022-12-09 11:49:07 +0600

    Update Zip method.py

--- Fundamental/Recap/Zip method.py
+++ Fundamental/Recap/Zip method.py
@@ -1,1 +1,12 @@
 
+# ---------------------------------- Example 01 -------------------------
+
+nums = (1,2,3)
+string = ['one','two','three']
+num = [1.1,2.2,3.3]
+
+new_obj = zip(nums,string,num)
+print(list(new_obj))
+# output : [(1, 'one', 1.1), (2, 'two', 2.2), (3, 'three', 3.3)]
+
+
