aeee179c1a17382a897eb9aff6ddaa0ae23f1e75
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2022-12-11 12:10:46 +0600

    Update Abstract Class.py

--- Fundamental/Recap/Abstract Class.py
+++ Fundamental/Recap/Abstract Class.py
@@ -1,1 +1,17 @@
 
+#------------------------------------------------->ABSTRACT CLASS <------------------------------------------
+
+"""  
+To create blue-print of object we use class
+
+ABSTRACT-CLASS : To create a blue-print of a class, we use abstract class. It is not possible to create object of an abstract class.
+
+ABSTRACT-MEHTOD : if we declare a function/method but do not perform any task the it is called abstract method
+"""
+
+#-------------- Normal class ----------------
+class Vehicle:
+    def run(self):
+        print('Vehicle is running......')
+car = Vehicle()
+car.run() #output : Vehicle is running
