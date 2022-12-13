0aa6d01de3a0c50f94e4729e3ebd4665603423c1
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2022-12-13 23:43:01 +0600

    Update Try_except_else_finllay.py

--- Fundamental/Recap/Try_except_else_finllay.py
+++ Fundamental/Recap/Try_except_else_finllay.py
@@ -2,3 +2,8 @@
 """  
 If any chances to found error in our code then we use try except method.
 In Try part , we write our desigerd code and except part, we print possible error name.if any error found then it print except part.
+
+we can use another two keyword with try and except those are else and finally.
+else-when the code did not show any error then the else part will appear.
+Finally- the finally part always runnded, if error found or not.
+"""
