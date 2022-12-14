f41c05c6af19bd688359f54bb0b583c519bba8fc
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2022-12-14 23:57:49 +0600

    Update Class method.py

--- Fundamental/Recap/Class method.py
+++ Fundamental/Recap/Class method.py
@@ -1,1 +1,4 @@
 
+"""  
+If any chances to found error in our code then we use try except method.
+In Try part , we write our desigerd code and except part, we print possible error name.if any error found then it print except part.
