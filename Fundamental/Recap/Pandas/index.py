e09ce02d0306609bfac486890a3845969852615c
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2022-12-16 23:56:37 +0600

    Update index.py

--- Fundamental/Recap/Pandas/index.py
+++ Fundamental/Recap/Pandas/index.py
@@ -3,3 +3,6 @@ import pandas as pt
 
 df = pt.read_csv("diabetes.csv", index_col='Age')
 print(df.describe()) # it shows--> count,mean,std,min,25%,50%,75%,max of all data.
+
+df_head = df.head()
+print(df_head) #it shows only first 5 data from all data
