64c5d557c558ecbecbdbb219fe263ca201d80f9c
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2022-12-16 23:56:52 +0600

    Update index.py

--- Fundamental/Recap/Pandas/index.py
+++ Fundamental/Recap/Pandas/index.py
@@ -6,3 +6,10 @@ print(df.describe()) # it shows--> count,mean,std,min,25%,50%,75%,max of all dat
 
 df_head = df.head()
 print(df_head) #it shows only first 5 data from all data
+
+
+print(df_head.groupby('HighBP').sum()) #sum for individual value
+
+print(df_head.groupby('HighBP').mean()) #mean for individual value
+
+print(df_head['CholCheck'].value_counts()) # how many types of number & how many number in same type in a column CholChek,it shows
