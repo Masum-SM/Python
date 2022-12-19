b897b4a8b5b19a3ba389cf6adb858af77e608359
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2022-12-19 23:53:03 +0600

    Create 1st.py

---
+++ Application/Final/1st.py
@@ -0,0 +1,39 @@
+data={
+    'a':[{
+        'aa':{'aax':5,'aay':6,'aaz':7},
+        'ab':{'abx':8,'aby':9,'abz':10}
+        },
+        {
+        'aaa':{'aaax':11,'aaay':12,'aaaz':13},
+        'aab':{'aabx':14,'aaby':15,'aabz':16}
+        }],
+    'b':[{
+        'ba':{'bax':17,'bay':18,'baz':19},
+        'bb':{'bbx':20,'bby':21,'bbz':22}
+        },
+        {
+        'bba':{'bbax':23,'bbay':24,'bbaz':25},
+        'bbb':{'bbbx':26,'bbby':27,'bbbz':28}
+        }],
+    'c':[{
+        'ca':{'cax':29,'cay':30,'caz':31},
+        'cb':{'cbx':32,'cby':33,'cbz':34}
+        },
+        {
+        'cca':{'ccax':35,'ccay':36,'ccaz':37},
+        'ccb':{'ccbx':38,'ccby':39,'ccbz':40}
+        }]
+}
+
+"""  
+You have the data given below. You need to write a python program to print as sample output from the data
+
+
+Output:
+
+Key:aax Value: 5
+Key:aay Value: 6
+Key:aaz Value: 7
+Key:abx Value: 8
+
+"""
