a811388e6bdb62eb076d86dca115038f78a3bc98
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2022-12-20 23:48:15 +0600

    Create python scope.py

---
+++ Application/Final/python scope.py
@@ -0,0 +1,8 @@
+""" There are four types of scopes in python
+Local Scope: A variable that is inside a function is determined as local scope of that function, and we can not access that varible outside the function. 
+
+Global Scope: A variable that is in the main body of the Python code is a global variable and belongs to the global scope. It is possible to access global variable at anywhere from the program.
+
+Built-in Scope: Built-in scope is a special Python scope that’s created or loaded whenever you run a script or open an interactive session.
+Enclosing Scope: Enclosing function can exists for nested function and it is special type of scope.
+ """
