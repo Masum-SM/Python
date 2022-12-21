be28d05f9232b63269000dd699f4ddf14438eb44
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2022-12-21 10:11:50 +0600

    Update number_pattern.py

--- Application/Final/number_pattern.py
+++ Application/Final/number_pattern.py
@@ -10,3 +10,14 @@ When n=5,
 23451
 
 """
+
+
+number = int(input("Enter a integer number: "))
+for i in range(1, number+1):
+    j = i
+    for k in range(2, j+1):
+        print(k, end='')
+    print(1, end='')
+    for k in range(j+1, number+1):
+        print(k, end='')
+    print('')
