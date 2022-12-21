08ec2d9408d7384956324c1b5d68f1153db444d0
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2022-12-21 10:11:34 +0600

    Update number_pattern.py

--- Application/Final/number_pattern.py
+++ Application/Final/number_pattern.py
@@ -1,1 +1,12 @@
 
+"""  
+You need to write a python program to take an integer input from the user and print a pattern given below.									10
+
+When n=5,
+12345
+21345
+23145
+23415
+23451
+
+"""
