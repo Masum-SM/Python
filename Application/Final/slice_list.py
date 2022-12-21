0ab2be39c3d7057a6b4129dc14d5030647e00b71
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2022-12-21 10:13:26 +0600

    Update slice_list.py

--- Application/Final/slice_list.py
+++ Application/Final/slice_list.py
@@ -1,1 +1,9 @@
 
+"""  
+You need to slice the list with values which are divided by 3. The list is given below.    5
+ 
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+ 
+Output:
+[3, 6, 9, 12, 15, 18]
+"""
