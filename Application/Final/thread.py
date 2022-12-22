19ac52a2f4ec50e575e4dc7641ec1b1da7aa01f8
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2022-12-22 11:59:07 +0600

    Update thread.py

--- Application/Final/thread.py
+++ Application/Final/thread.py
@@ -1,3 +1,23 @@
 """  
 Run the functions in 4 threads.
 """
+
+from time import perf_counter
+from threading import Thread
+start_time = perf_counter()
+
+def f1():
+    for i in range(5):
+        print(f"f1 - {i}")
+ 
+def f2():
+    for i in range(5):
+        print(f"f2 - {i}")
+ 
+def f3():
+    for i in range(5):
+        print(f"f3 - {i}")
+       
+def f4():
+    for i in range(5):
+        print(f"f4 - {i}")
