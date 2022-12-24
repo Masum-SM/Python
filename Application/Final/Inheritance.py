af3d92755b79d3066012901e66b15ccb4599f8d3
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2022-12-24 11:43:24 +0600

    Update Inheritance.py

--- Application/Final/Inheritance.py
+++ Application/Final/Inheritance.py
@@ -1,3 +1,14 @@
 """ 
 Single Inheritence: Single inheritance enables a derived class to inherit properties from a single parent class, enabling code reusability and the addition of new features to existing code.
 """
+
+class Mouse: # Base class
+    def __init__(self,brand,wire_type,color) -> None:
+        self.brand = brand
+        self.wire_type = wire_type
+        self.color = color
+
+class Logitech(Mouse): #Derived class
+    def __init__(self, brand, wire_type, color,price) -> None:
+        self.price = price
+        super().__init__(brand, wire_type, color) # This is supper function that allow the derived class to access attribute and method of base class.
