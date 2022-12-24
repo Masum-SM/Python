7f9f1dce8b082cdd5bdb69e2e3fb2c3d5c8f92da
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2022-12-24 11:42:46 +0600

    Create Inheritance.py

---
+++ Application/Final/Inheritance.py
@@ -0,0 +1,3 @@
+""" 
+Single Inheritence: Single inheritance enables a derived class to inherit properties from a single parent class, enabling code reusability and the addition of new features to existing code.
+"""
