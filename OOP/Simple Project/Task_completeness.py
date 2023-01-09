5b8816d55d946e4f2665950bbc46aed3550f4448
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2023-01-09 22:51:05 +0600

    Update Task_completeness.py

--- OOP/Simple Project/Task_completeness.py
+++ OOP/Simple Project/Task_completeness.py
@@ -1,1 +1,15 @@
 
+from datetime import datetime
+from uuid import uuid4
+
+class Task:
+    all_task = []
+    new_task_list = []
+    def __init__(self) -> None:
+        self.id = str(uuid4())
+        self.Task_Name = "";
+        self.Created_time = datetime.now().strftime("%d/%m/%Y %H:%M:%S");
+        self.Updated_time = "NA"
+        self.Task_done = False
+        self.Completed_time= "NA"
+        
